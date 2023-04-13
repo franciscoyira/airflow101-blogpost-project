6ca08052bc2a0656ede2c9a9306f94c810ec89ed
Francisco Yirá Albornoz <francisco.yira@outlook.com>
2023-04-12 20:08:29 -0400

    this DAG runs

--- dags/dag_pomodoro.py
+++ dags/dag_pomodoro.py
@@ -1,12 +1,15 @@
 from airflow import DAG
+from airflow.models import DagRun
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+from s3fs import S3FileSystem
+from pandas import read_csv, concat
 import boto3
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 4, 11),
+    'start_date': datetime(2023, 4, 6),
     'email_on_failure': 'francisco.yira@outlook.com',
     'email_on_retry': 'francisco.yira@outlook.com',
     'retries': 1,
@@ -22,7 +25,7 @@ dag = DAG(
     schedule="@weekly"
 )
 
-def copy_latest_file_from_s3():
+def read_new_files_from_bucket():
     # Create a boto3 session to interact with S3/AWS
     session = boto3.Session()
     s3 = session.client('s3')
@@ -32,18 +35,33 @@ def copy_latest_file_from_s3():
     # this returns a dict (within a list) with the names and properties of
     # the objects in our bucket
 
-    #  Find the latest modified object
-    latest_object = max(objects, key=lambda x: x['LastModified'])
+    # It's [-2] because [-1] is the current dag run
+    last_dag_run = DagRun.find(dag_id='copy_latest_file_from_s3')[-2]
+    last_dag_run_date = last_dag_run.execution_date
+    print('Last dag run date: ', last_dag_run_date)
+
+    # For the objects that are more recent than the last dag run date,
+    # reads them (using read_csv) and combines in them in a pandas dataframe
+    dfs = []
+    for obj in objects:
+        print('Last modified object: ', obj['LastModified'])
+        if obj['LastModified'] > last_dag_run_date:
+            dfs.append(read_csv('s3://pomodoro-sessions/' + obj['Key']))
     
-    # Copy the object to the destination bucket
-    s3.copy_object(
-        CopySource={'Bucket': 'pomodoro-sessions',
-                    'Key': latest_object['Key']},
-                    Bucket='processed-pomodoro-sessions',
-                    Key=latest_object['Key']
-                    )
+    if len(dfs) > 0:
+        df_combined = concat(dfs, axis=0)
+
+        # S3 key for the file that informs the date
+        date_str = datetime.now().strftime('%Y-%m-%d')
+        key = f'processed_{date_str}.csv'
+
+        # write df_combined as CSV on the S3 bucket
+        s3.put_object(
+            Body=df_combined.to_csv(),
+            Bucket='processed-pomodoro-sessions',
+            Key=key)
 
 t1 = PythonOperator(
         task_id='copy_latest_file_from_s3',
-        python_callable=copy_latest_file_from_s3,
+        python_callable=read_new_files_from_bucket,
         dag=dag)
